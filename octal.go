380fc8e52e4386160ba5b2d253b1f08e227d02e7
eliss <staaf.elis@gmail.com>
2024-10-29 14:14:49 +0100

    Break up into organized project structure.
    [UTILS/UTILS.GO] <- Contains all of the functions
    [OCTAL.GO] <- Straps all of the functions together

--- octal.go
+++ octal.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
+    "octal/utils"
 )
-
-func readLines(path string) ([]string, error) {
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
-
-    var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-    return lines, scanner.Err()
-}
-
-func count(filename string) (int, int) {
-    lines, err := readLines(filename)
-    if err != nil {
-        fmt.Println("[ERROR]: bufio.NewScanner->readLines() failed.")
-    }
-
-    var loc int = 0
-    var empty int = 0
-
-    for i := 0; i < len(lines); i++ {
-        var line string = lines[i]
-        if len(strings.TrimSpace(line)) == 0 {
-            empty++
-            continue
-        }
-        loc++
-    }
-
-    return loc, empty
-}
 
 func main() {
     if len(os.Args) < 2 {
         fmt.Println("usage: octal <file>")
         os.Exit(0)
     }
-    loc, empty := count(os.Args[1])
+    loc, empty := utils.CLOC(os.Args[1])
     fmt.Println(os.Args[1:])
     fmt.Printf("LOC: %d\n", loc)
     fmt.Printf("Empty: %d\n", empty)
---
+++ utils/utils.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ReadLines(path string) ([]string, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
+}
+
+func CLOC(filename string) (int, int) {
+    lines, err := ReadLines(filename)
+    if err != nil {
+        fmt.Println("[ERROR]: bufio.NewScanner->readLines() failed.")
+    }
+
+    var loc int = 0
+    var empty int = 0
+
+    for i := 0; i < len(lines); i++ {
+        var line string = lines[i]
+        if len(strings.TrimSpace(line)) == 0 {
+            empty++
+            continue
+        }
+        loc++
+    }
+
+    return loc, empty
+}
